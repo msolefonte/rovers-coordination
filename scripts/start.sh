99948768ff7322afc0d1f10e08801c3e9421b7d8
Marc Solé <msolefonte@gmail.com>
2021-11-23 15:50:06 +0000

    Add Rovers
    Co-authored-by: msolefonte <19@370W^PpNs>

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,14 +2,14 @@
 
 mkdir -p /tmp/p3/logs
 
-python3 lib/coordinator.py localhost 7000 localhost:7001,localhost:7002 > /tmp/p3/logs/c0.log &
-python3 lib/coordinator.py localhost 7001 localhost:7002,localhost:7000 > /tmp/p3/logs/c1.log &
-python3 lib/coordinator.py localhost 7002 localhost:7000,localhost:7001 > /tmp/p3/logs/c2.log &
-python3 lib/peer.py node0 localhost 9000 localhost:7000,localhost:7001,localhost:7002 node1,node2 > /tmp/p3/logs/p0.log &
-python3 lib/peer.py node1 localhost 9001 localhost:7000,localhost:7001,localhost:7002 node2,node3 > /tmp/p3/logs/p1.log &
-python3 lib/peer.py node2 localhost 9002 localhost:7000,localhost:7001,localhost:7002 node3,node4 > /tmp/p3/logs/p2.log &
-python3 lib/peer.py node3 localhost 9003 localhost:7000,localhost:7001,localhost:7002 node4,node5 > /tmp/p3/logs/p3.log &
-python3 lib/peer.py node4 localhost 9004 localhost:7000,localhost:7001,localhost:7002 node5,node6 > /tmp/p3/logs/p4.log &
-python3 lib/peer.py node5 localhost 9005 localhost:7000,localhost:7001,localhost:7002 node6,node7 > /tmp/p3/logs/p5.log &
-python3 lib/peer.py node6 localhost 9006 localhost:7000,localhost:7001,localhost:7002 node7,node0 > /tmp/p3/logs/p6.log &
-python3 lib/peer.py node7 localhost 9006 localhost:7000,localhost:7001,localhost:7002 node0,node1 > /tmp/p3/logs/p7.log &
+hostname=$(hostname -I | awk '{print $1}')
+
+python3 lib/rover.py rover0 ${hostname} 7000 ${hostname}:7001,${hostname}:7002,${hostname}:7003,${hostname}:7004,${hostname}:7005,${hostname}:7006,${hostname}:7007,${hostname}:7008 0,0,999,999 20 1500 > /tmp/p3/logs/rover0.log &
+python3 lib/rover.py rover1 ${hostname} 7001 ${hostname}:7000,${hostname}:7002,${hostname}:7003,${hostname}:7004,${hostname}:7005,${hostname}:7006,${hostname}:7007,${hostname}:7008 0,1000,999,1999 20 1500 > /tmp/p3/logs/rover1.log &
+python3 lib/rover.py rover2 ${hostname} 7002 ${hostname}:7000,${hostname}:7001,${hostname}:7003,${hostname}:7004,${hostname}:7005,${hostname}:7006,${hostname}:7007,${hostname}:7008 0,2000,999,2999 20 1500 > /tmp/p3/logs/rover2.log &
+python3 lib/rover.py rover3 ${hostname} 7003 ${hostname}:7000,${hostname}:7001,${hostname}:7002,${hostname}:7004,${hostname}:7005,${hostname}:7006,${hostname}:7007,${hostname}:7008 1000,0,1999,999 20 1500 > /tmp/p3/logs/rover3.log &
+python3 lib/rover.py rover4 ${hostname} 7004 ${hostname}:7000,${hostname}:7001,${hostname}:7002,${hostname}:7003,${hostname}:7005,${hostname}:7006,${hostname}:7007,${hostname}:7008 1000,1000,1999,1999 20 1500 > /tmp/p3/logs/rover4.log &
+python3 lib/rover.py rover5 ${hostname} 7005 ${hostname}:7000,${hostname}:7001,${hostname}:7002,${hostname}:7003,${hostname}:7004,${hostname}:7006,${hostname}:7007,${hostname}:7008 1000,2000,1999,2999 20 1500 > /tmp/p3/logs/rover5.log &
+python3 lib/rover.py rover6 ${hostname} 7006 ${hostname}:7000,${hostname}:7001,${hostname}:7002,${hostname}:7003,${hostname}:7004,${hostname}:7005,${hostname}:7007,${hostname}:7008 2000,0,2999,999 20 1500 > /tmp/p3/logs/rover6.log &
+python3 lib/rover.py rover7 ${hostname} 7007 ${hostname}:7000,${hostname}:7001,${hostname}:7002,${hostname}:7003,${hostname}:7004,${hostname}:7005,${hostname}:7006,${hostname}:7008 2000,1000,2999,1999 20 1500 > /tmp/p3/logs/rover7.log &
+python3 lib/rover.py rover8 ${hostname} 7008 ${hostname}:7000,${hostname}:7001,${hostname}:7002,${hostname}:7003,${hostname}:7004,${hostname}:7005,${hostname}:7006,${hostname}:7007 2000,2000,2999,2999 20 1500 > /tmp/p3/logs/rover8.log &
