31e7ed0bb2ef861f2273cc9da9d0d05257b1a180
Marc Solé <msolefonte@gmail.com>
2021-11-29 20:48:38 +0000

    Update rover.py

--- lib/rover.py
+++ lib/rover.py
@@ -4,7 +4,7 @@ from time import sleep
 
 
 def main():
-    print('[INFO] Rover started with args ', sys.argv[1:], flush=True)
+    print('[INFO] Rover started with args', sys.argv[1:], flush=True)
     rover_id, host, port, known_peers, coordinates, speed, radio_range = sys.argv[1:]
     rover = Rover(rover_id, host, int(port), known_peers, coordinates, int(speed), int(radio_range))
     rover.start()
