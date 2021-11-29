8383212d5024efe46d525c295be57f43bfbf74d8
msolefonte <19@370W^PpNs>
2021-11-29 19:09:24 +0000

    Remove non encryption related changes

--- lib/rover.py
+++ lib/rover.py
@@ -1,7 +1,6 @@
 import sys
 from Rover import Rover
 from time import sleep
-from EarthStation import EarthStation
 
 
 def main():
@@ -9,16 +8,13 @@ def main():
     rover_id, host, port, known_peers, coordinates, speed, radio_range = sys.argv[1:]
     rover = Rover(rover_id, host, int(port), known_peers, coordinates, int(speed), int(radio_range))
     rover.start()
-    
 
     while True:
         sleep(5)
         try:
             rover.heartbeat()
-            if rover_id == 'rover3':
-                rover.broadcast_request('hello','rover5')
-                rover.broadcast_request('hello_earth','earth-station')
-                
+            if rover_id == 'rover8':
+                rover.broadcast_message_to('hello', 'rover0')
         except SystemError:
             pass  # Networking disabled due to low battery in the rover
 
--- lib/utils/SDNNode.py
+++ lib/utils/SDNNode.py
@@ -5,10 +5,7 @@ import threading
 import uuid
 
 
-
 class SDNNode:
-
-
     def __init__(self, node_id, location, host, port, known_peers, radio_range):
         # Identification
         self.node_id = node_id
@@ -26,20 +23,12 @@ class SDNNode:
         self.networking_disabled = False
         self.consumed_nonces = {}  # {nonce: timestamp}
 
-
     # Simulation
 
     def _is_too_far_away(self, location):
-        if location == 'Earth':
-            return False
-        else:
-            return ((((location['x'] - self.location['x'])**2) + ((location['y']-self.location['y'])**2))**0.5) > \
+        return ((((location['x'] - self.location['x'])**2) + ((location['y']-self.location['y'])**2))**0.5) > \
                self.radio_range
 
-
-   
-
-    
     @staticmethod
     def _send_message_to_known_peer(host, port, message, tries):
         for i in range(tries):
@@ -50,9 +39,7 @@ class SDNNode:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
                     soc.connect((host, int(port)))
                     soc.sendall(str.encode(message))
-                    #soc.sendall(SDNNode.encrypt_message(message,__class__.private_key,__class__.private_iv)) #Manu
                     data = soc.recv(1024).decode()
-                    #data = SDNNode.decrypt_message(soc.recv(1024),__class__.private_key,__class__.private_iv)[1] #Manu
                     if data == 'TOO_FAR_AWAY':  # Simulates it is out of physical range. Assume connection refused.
                         raise ConnectionError('Connection refused by ', host + ':' + port)
             except ConnectionError:
@@ -89,14 +76,11 @@ class SDNNode:
                 try:
                     data = connection.recv(1024)
                     message = json.loads(data.decode())
-                    #message = json.loads(SDNNode.decrypt_message(data,__class__.private_key,__class__.private_iv)[1]) #Manu
                     if data and message['location']:
                         # Simulates it is out of physical range. Basically refuses connection.
                         if self.networking_disabled or self._is_too_far_away(message['location']) :
                             connection.sendall('TOO_FAR_AWAY'.encode())
-                            #connection.sendall(SDNNode.encrypt_message('TOO_FAR_AWAY',__class__.private_key,__class__.private_iv))  #Manu
                         else:
-                            
                             threading.Thread(
                                 target=lambda: self._handle_request(message, client_address)).start()
                 finally:
@@ -119,7 +103,7 @@ class SDNNode:
                 ).start()
         else:
             raise SystemError('Broadcasting disabled')
-    
+
     def broadcast_message_to(self, message, target_id, reply_to_id=None, nonce=None, ttl=16):
         if not self.networking_disabled:
             nonce = nonce if nonce else uuid.uuid4().hex
@@ -142,6 +126,3 @@ class SDNNode:
 
     def heartbeat(self):
         self.broadcast(json.dumps({'type': 'heartbeat'}))
-
-    
-    
